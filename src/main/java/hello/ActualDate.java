0b0b73a0e8b6b318f7be8734602903f9eab3a8a7
Gabriel Meirelles <gabrielmeirelles@id.uff.br>
2021-11-09 01:08:29 -0300

    correcoes pt5

--- src/main/java/hello/ActualDate.java
+++ src/main/java/hello/ActualDate.java
@@ -28,35 +28,47 @@ public class ActualDate implements Serializable {
     
     public void setDate(String value) {
         Date currentDate = Calendar.getInstance().getTime();  
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
-        String strDate = dateFormat.format(currentDate);
-
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
+        
+        String strDate;
         String dateText;
         
         switch (value){
             case "":
             case "pt":
                 dateText = "Data e horário atual no Brasil: ";
+                dateFormat.setTimeZone(TimeZone.getTimeZone("BRT"));
+                strDate = dateFormat.format(currentDate); 
                 date = dateText + strDate;
                 break;
             case "en":
                 dateText = "Current date and time in USA: ";
+                dateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
+                strDate = dateFormat.format(currentDate); 
                 date = dateText + strDate;
                 break;
             case "de":
                 dateText = "Aktuelles Datum und Uhrzeit in Deutschland: ";
+                dateFormat.setTimeZone(TimeZone.getTimeZone("CET"));
+                strDate = dateFormat.format(currentDate); 
                 date = dateText + strDate;
                 break;
             case "fr":
                 dateText = "Date et heure actuelles en France: ";
+                dateFormat.setTimeZone(TimeZone.getTimeZone("CET"));
+                strDate = dateFormat.format(currentDate); 
                 date = dateText + strDate;
                 break;
             case "se":
                 dateText = "Aktuellt datum och tid i Sverige: ";
+                dateFormat.setTimeZone(TimeZone.getTimeZone("CET"));
+                strDate = dateFormat.format(currentDate); 
                 date = dateText + strDate;
                 break;
             case "in":
                 dateText = "bhaarat mein vartamaan tithi aur samay: ";
+                dateFormat.setTimeZone(TimeZone.getTimeZone("IST"));
+                strDate = dateFormat.format(currentDate); 
                 date = dateText + strDate;
                 break;
         }
